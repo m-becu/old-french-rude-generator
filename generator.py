38cfadf99bb83e8d3fb678102d65fd16299c88b0
Anorak <mbecumetailler@gmail.com>
2020-07-10 02:19:11 +0200

    generator v1

---
+++ generator.py
@@ -0,0 +1,32 @@
+from random import randint
+
+rude_list = [
+    'Pauvre cloche',
+    'Cher vieux débris',
+    'Misérable loque humaine',
+    'Vieux shnoque',
+    'Séducteur déplumé',
+    'Vieux macaque édenté',
+    'Noble Jean-Foutre',
+    'Chère vieille crapule',
+    'Vieille charogne',
+    'Pâle escroc',
+    'Bougre de galapiat',
+    'Vieux pourri',
+    'Méprisable ordure',
+    'Sinistre individu',
+    'Vieux machin',
+    'Vieillard cacochyme',
+    'Petit foutriquet',
+    'Vieux matou',
+    'Gros malpropre',
+    'Greluchon famélique',
+    'Petit voyou',
+    'Gibier de potence'
+]
+
+def generate():
+    print(rude_list[randint(0,len(rude_list))])
+
+if __name__ == "__main__":
+    generate()
